6a2f48f0d54e1a99a0ea38a80060a8c21e059ba0
lost1k <only.lost1k@gmail.com>
2022-12-05 20:10:22 +0300

    add fill array and print array methods

--- homeTask/Program.cs
+++ homeTask/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая 
+// 1. из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// 2. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.Write("Введите размер массива : ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+string[] stringArray = new string[arrayLength];
+
+string[] InputArray(int size)
+{
+    string[] array = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) System.Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+
+
+stringArray = InputArray(arrayLength);
+PrintArray(stringArray);
