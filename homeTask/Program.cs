248cf217e7193b48112704850808de9fc0d4ad76
lost1k <only.lost1k@gmail.com>
2022-12-05 20:27:44 +0300

    added sortedArray

--- homeTask/Program.cs
+++ homeTask/Program.cs
@@ -14,6 +14,11 @@ string[] InputArray(int size)
     for (int i = 0; i < size; i++)
     {
         array[i] = Console.ReadLine();
+        if (array[i].Length == 0)
+        {
+            Console.WriteLine("Введите значение заново : ");
+            array[i] = Console.ReadLine();
+        }
     }
     return array;
 }
@@ -23,13 +28,30 @@ void PrintArray(string[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if (i < array.Length - 1) System.Console.Write($"{array[i]},");
+        if (i < array.Length - 1) System.Console.Write($"{array[i]} ; ");
         else Console.Write($"{array[i]}");
     }
-    Console.WriteLine("]");
+    Console.Write("]");
+}
+
+void PrintSortedArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            if (i < array.Length - 1) Console.Write($" {array[i]} ; " );
+            else Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
 }
 
 
-
 stringArray = InputArray(arrayLength);
+Console.WriteLine();
 PrintArray(stringArray);
+Console.Write("=>");
+PrintSortedArray(stringArray);
+Console.ReadLine();     
